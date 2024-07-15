3bcc1ff319e6c4f7d1273b55e5ada0028a12fcdb
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-07-15 12:40:30 +0300

    Style the Currently Active Navigation Link

--- resources/views/Components/layouts.blade.php
+++ resources/views/Components/layouts.blade.php
@@ -20,9 +20,9 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-              <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-              <a href="/contacts" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contacts</a>
+              <x-nav-link href="/" :active="request()-> is('/')">Home</x-nav-link>
+              <x-nav-link href="/about" :active="request()-> is('about')">About</x-nav-link>
+              <x-nav-link href="/contacts" :active="request()-> is('contacts')">Contacts</x-nav-link>
             </div>
           </div>
         </div>
