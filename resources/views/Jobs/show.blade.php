30fbb68843bd72035361f6f193e808b3e52ea567
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-08-08 14:19:57 +0300

    Editing, Updating, and Deleting a Resource

--- resources/views/Jobs/show.blade.php
+++ resources/views/Jobs/show.blade.php
@@ -2,8 +2,12 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-<h1 class="font-bold text-lg"><strong>{{ $job['title']}}</strong></h1>
+<h1 class="font-bold text-lg"><strong>{{ $job->title}}</strong></h1>
 <p class="text-sm">
-    Pays a Salary of <strong>{{ $job['Salary']}}</strong> per year.
+    Pays a Salary of <strong>{{ $job->Salary}}</strong> per year.
 </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </p>
 </x-layouts>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/contacts', function () {
     return view('contacts');
 });
 
+// Index (Displays All Jobs)
 Route::get('/jobs', function () {
    $jobs = Job::with('employer')->latest()->paginate(30);
     //$jobs = Job::with('employer')->simplePaginate(30);
@@ -27,10 +28,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//Create (Displays a Form To Create a job)
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//Store(Stores a New Job To The DataBase)
 Route::post('/jobs', function () {
     request()->validate([
         /*
@@ -47,9 +50,61 @@ Route::post('/jobs', function () {
     ]);
     return redirect('/jobs');
 });
+
+//Show (Displays A Page To show The Job)
 Route::get('/jobs/{id}', function ( $id) {
   $job = Job::find($id);
    // $job=Arr::first( job::all(), fn($job) => $job['id'] == $id); //Search the job that has the matching id
 
     return view('jobs.show',['job'=> $job]);// when the job is found it is loaded to the view
+});
+
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job= Job:: find($id);
+
+    return view('jobs.edit',['job'=> $job]);
+});
+
+//Edit
+
+//Update
+Route::patch('/jobs/{id}', function ($id) {// Patch-Is A Set Of instructions for how to Modify Resources
+    //Validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'Salary' => ['required']
+    ]);
+
+    //Authorize (On Hold...)
+
+    //Update the Job
+    $job = Job::findOrFail($id);
+
+/*
+    $job->title = request('title');
+    $job->Salary = request('Salary');
+    $job->save();
+*/
+
+    //Another way of Updating
+    $job->update([
+        'title' => request('title'),
+        'Salary' => request('Salary')
+    ]);
+
+    //Redirect
+    return redirect('/jobs/' . $job->id );
+});
+
+
+//Destroy
+Route::delete('/jobs/{id}', function ($id) {// Delete-Remove The Specified Resource
+// Authorize(On Hold...)
+
+    //Delete The Job
+    $job = Job::findOrFail($id);
+    $job->delete();
+
+    //Redirect
+    return redirect('/jobs');
 });
