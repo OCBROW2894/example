40378dc887fcb0fc2f1aa85a6226041508f86f10
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-08-15 14:40:37 +0300

    6 Steps to Authorization Mastery (4/6(2/3))

--- resources/views/Jobs/show.blade.php
+++ resources/views/Jobs/show.blade.php
@@ -7,7 +7,9 @@
     Pays a Salary of <strong>{{ $job->Salary}}</strong> per year.
 </p>
 
+    @can('edit-job', $job)
     <p class="mt-6">
         <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
     </p>
+    @endcan
 </x-layouts>
