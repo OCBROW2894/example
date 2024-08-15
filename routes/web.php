3962862d6b5f117a8c784364f9da2ab17c66eda5
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-08-15 15:05:18 +0300

    6 steps to Authorization Mastery (5/6)

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,26 @@ Route::view('/', 'welcome', [
 ]);
 Route::view('/about', 'about');
 Route::view('/contacts', 'contacts');
-
+/*
 //Resource gets all items route that are in the itemController (jobs, JobContoller)
-Route::resource('jobs', JobController::class);
+Route::resource('jobs', JobController::class)->only(['index', 'show']);
+Route::resource('jobs', JobController::class)->except(['index', 'show'])->middleware('auth');
+*/
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+Route::get('/jobs/{job}', [JobController::class, 'show']);
 
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit-job', 'job');
+
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 //Auth
 Route::get('/register', [RegisteredUserController::class,'create']);
 Route::post('/register', [RegisteredUserController::class,'store']);
 
-Route::get('/login',[SessionController::class,'create']);
+Route::get('/login',[SessionController::class,'create'])->name('login');
 Route::post('/login',[SessionController::class,'store']);
 Route::post('/logout',[SessionController::class,'destroy']);
